7d4d5eb3f291377b0de254ee73e982e210449903
Lenin Yee <leninyee@gmail.com>
2014-04-08 12:03:06 -0600

    Added aliases gds and gdc

--- aliases.bash
+++ aliases.bash
@@ -3,5 +3,7 @@ alias vi=vim
 # Git
 alias g='git status'
 alias gd='git diff'
+alias gds='git diff --staged'
 alias gci='git commit -v'
 alias gl='git log'
+alias gcd='git checkout develop && git pull'
